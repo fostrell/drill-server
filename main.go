50a846828e1ffe1379be87e0a49a9c736f755c9e
Andrey <andrej.savchuk@gmail.com>
2019-12-30 21:41:33 +0100

    http server

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/data", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	dat, _ := ioutil.ReadFile("./static/data.json")
+	fmt.Fprint(w, string(dat))
 }
